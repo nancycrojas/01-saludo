2b8dd1beb03e18ece77e728e61655da6d19081b4
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 08:29:26 -0300

    Variables, datos y operadores - ejercicio 1

---
+++ app.js
@@ -0,0 +1,9 @@
+//Saludo
+
+//Crear un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
+
+
+const nombre = prompt ("Por favor ingresa tu nombre");
+const apellido = prompt ("Por favor ingresa tu apellido")
+alert("Hola " + nombre + " " + apellido + ", " + "bienvenida a Ada")
+
