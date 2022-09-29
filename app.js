7b6e0d8b2317adb031c3ed2f8f6b912e16cf5b94
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 15:32:15 -0300

    Agregando template strings

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// Variables, datos y operadores
+// Ejercicios
+// Cuando la consigna pida ingresar algo hacerlo mediante un prompt, y cuando pida mostrar un mensaje hacerlo mediante alert. Usar const, let y template strings.
+
 //Saludo
 
 //Crear un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
 
 
-const nombre = prompt ("Por favor ingresa tu nombre");
-const apellido = prompt ("Por favor ingresa tu apellido")
-alert("Hola " + nombre + " " + apellido + ", " + "bienvenida a Ada")
+const nombre = prompt("Por favor ingresa tu nombre");
+const apellido = prompt("Por favor ingresa tu apellido")
+//alert("Hola " + nombre + " " + apellido + ", " + "bienvenida a Ada");
+
+
+//Template strings
+alert(`Hola ${nombre} ${apellido}, bienvenida a Ada.`);
 
